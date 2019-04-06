bdfcfdc941e52d9c79d55c5eab9517a15574dfc3
Arin Trongsantipong <arintrongs@gmail.com>
2019-04-06 23:37:22 +0700

    First First commit

---
+++ scraping/the_standard/the_standard/spiders/news.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class NewsSpider(scrapy.Spider):
+    name = 'news'
+    # allowed_domains = ['thestandard.co']
+    start_urls = ['https://thestandard.co/category/news/page/' +
+                  str(page_num) for page_num in range(1, 966)]
+
+    def parse(self, response):
+        for url in response.xpath('//h3[@class=\'news-title\']/a/@href').getall():
+            yield scrapy.Request(url, callback=self.news)
+
+    def news(self, response):
+        headline = response.xpath(
+            '//div[@class=\'entry-title\']/h1/text()').get().strip('"').strip()
+        date = response.xpath(
+            '//div[@class=\'meta-date\']/span/text()').get().strip('"').strip()
+        views = response.xpath(
+            '//span[@class=\'entry-view\']/@data-views').get().strip('"').strip()
+        share = response.xpath(
+            '//span[@class=\'fb-share-count count\']/text()').get().strip('"').strip()
+        category = response.xpath(
+            '//div[@class=\'entry-meta\']/span[@class=\'category\']/a/text()').getall()
+        tags = response.xpath('//div[@class=\'tags\']/a/text()').getall()
+        contents = response.xpath(
+            '//div[@class=\'entry-content\']/p/span/text()').getall()[:-1]
+        contents = ''.join(contents)
+        yield {'headline': headline, 'date': date, 'time': None, 'view': views, 'like': None, 'share': share, 'comment': None, 'category': category, 'tag': tags, 'content': contents}
